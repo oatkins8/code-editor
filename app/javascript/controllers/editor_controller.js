65cf2a0197ab1d9e965fd38b24c94ddc1bb51781
Ollie Atkins <ollieatkins8@outlook.com>
2025-09-09 08:59:29 +0100

    Add codemirror import statements
    As we're using esbuild, the resolution of JS module locations happens
    during a build step (e.g rails assets:precompile).
    
    Here, we need to import packages from the codemirror node modules
    installed earlier via yarn.
    
    import statements are a common pattern for loading third-party libraries
    into a stimulus controller.

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,4 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
+import { basicSetup } from "codemirror"
+import { EditorView } from "@codemirror/view"
 
 // Connects to data-controller="controller"
 export default class extends Controller {
